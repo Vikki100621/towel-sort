f9d32bddd1eb7df84386081f7bf42fa85cd8dc42
Viktoriya <kruckovaviktoria421@gmail.com>
2023-02-21 02:01:13 +0300

    init: create function to js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == [] || matrix == undefined) {
+    return []
+  } else {
+  return matrix.map ((item, index) => {  
+    if (index % 2 === 0) {
+        item = item 
+    } else  {
+     item = item.reverse();
+   }
+    return item;
+}).flat();
 }
+
+}
+
+
